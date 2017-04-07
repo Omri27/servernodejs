cc67f1a0c02cf103acdea24226d13f8a08717bd7
Omri <omri.kodzidlo@gmail.com>
2017-04-07 21:31:35 +0300

    get history runs datecomparison

--- functions/database-functions.js
+++ functions/database-functions.js
@@ -3,7 +3,7 @@
  */
 var admin = require("firebase-admin");
 var serviceAccount = require("../chatapp-d3713-firebase-adminsdk-mgk41-56a40c4550.json");
-
+var dateFormat = require('dateformat');
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://chatapp-d3713.firebaseio.com"
@@ -13,6 +13,7 @@ var db = admin.database();
 
 //public FeedListRequest(String title, String userId, String userName) {
 exports.getHistoryRuns= function(userId,callback){
+    var  nowDate = new Date();
     var usersRef = db.ref("/users/"+userId+"/comingUpRuns");
     var historyRunRef = db.ref("/users/"+userId+"/historyRuns");
     var historyRuns = [];
@@ -25,16 +26,26 @@ exports.getHistoryRuns= function(userId,callback){
                 runsRef.once("value",function(snapshot) {
                    var run= snapshot.val()
                     var historyRun =insertToClass(run);
-                   if(!snapshot.hasChild(key))
+                    var oldate= new Date(dateFormat(historyRun.date+ " "+historyRun.time, "dd-mm-yyyy hh:MM"));
+                   if(!snapshot.hasChild(key) && nowDate.getTime()>oldate.getTime())
                     historyRunRef.child(key).set(historyRun);
                 });
             });
 
         }
+        return callback.success;
     });
 
 };
 function insertToClass(run){
-    var historyClass ={date:run.date,time:run.time,creator:run.creator, location:run.location,distance:"",Preferences:run.preferences,maxRunners:"",marked:0,like:0};
+    var historyClass ={date:run.date,
+        time:run.time,
+        creator:run.creator,
+        location:run.location,
+        distance:"",
+        Preferences:run.preferences,
+        maxRunners:"",
+        marked:0,
+        like:0};
     return historyClass;
 }
