88742f9ce1f7ff88b6e61c010a00063a74267524
Omri <omri.kodzidlo@gmail.com>
2017-04-19 18:18:59 +0300

    CORS fix

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var bodyParser= require('body-parser');
 var DataBase = require("./functions/database-functions")
 
 var app= express();
+
 var port = process.env.PORT || 8080;
 app.use(bodyParser.json());
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,26 @@
 var sendFunction= require('../functions/send-message');
 var DataBase= require('../functions/database-functions');
+var cors = require('cors')
 module.exports= function(app){
+    app.options('*', cors());
+    app.use(function (req, res, next) {
+
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', '*');
+
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
+
+        // Pass to next layer of middleware
+        next();
+    });
 app.post('/send',function(req,res){
 	var message= req.body.message;
 	var registrationId= req.body.registrationId;
